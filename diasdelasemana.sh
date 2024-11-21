875591b72e526928131b07f3b572c05c85f219f7
lavara777 <rplavara@gmail.com>
2024-11-21 15:06:51 +0100

    examen 21 de noviembre

--- diasdelasemana.sh
+++ diasdelasemana.sh
@@ -7,4 +7,19 @@ mkdir semana1
 cd ./semana1
 mkdir miercoles jueves viernes sabado domingo
 cd ..
-
+mkdir semana2
+cd ./semana2
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+mkdir semana3
+cd ./semana3
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+mkdir semana3
+cd ./semana3
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+mkdir semana4
+cd ./semana4
+mkdir lunes martes miercoles jueves viernes
+cd ..
